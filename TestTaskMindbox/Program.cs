b75e719a205e3ec6509d5bf6aea54c5803e2dca4
Pavel <pvarets@gmail.com>
2023-04-16 22:10:03 +0300

    [test]

--- TestTaskMindbox/Program.cs
+++ TestTaskMindbox/Program.cs
@@ -8,3 +8,4 @@ context.SetContextShape(new Circle(4));
 context.GetResult();
 
 Console.WriteLine($"The squareness of the triangle is { new Triangle(6, 8, 10).CheckSquarenessTriangle()}");
+Console.WriteLine($"The squareness of the triangle is { new Triangle(6, 8, 10).CheckSquarenessTriangle()}");
